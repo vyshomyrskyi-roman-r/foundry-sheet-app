8a3c9a47f42aff93d288d75542856e87f5cfe575
Roman Vyshomyrskyi <vyshomyrskyi.roman@student.uzhnu.edu.ua>
2025-05-20 11:18:23 +0200

    log-out grid fix

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -3,18 +3,40 @@ import React from 'react';
 
 interface ButtonProps {
   children: React.ReactNode;
-  type?: 'button' | 'submit' | 'reset';
+  type?: "button" | "submit" | "reset";
+  agre?: boolean;
   onClick?: () => void;
 }
 
-export default function Button({ children, type = 'button', onClick }: ButtonProps) {
-  return (
-    <button
-      type={type}
-      onClick={onClick}
-      className="w-full bg-green-600 hover:bg-green-700 text-white text-lg py-2 rounded-full transition"
-    >
-      {children}
-    </button>
-  );
+
+
+export default function Button({
+  children,
+  type = "button",
+  agre = true,
+  onClick,
+}: ButtonProps) {
+  if (agre) {
+    return (
+      <button
+        type={type}
+        onClick={onClick}
+        className="bg-green-600 hover:bg-green-700 text-white text-lg py-2 rounded-full"
+        style={{ width: "-webkit-fill-available" }}
+      >
+        {children}
+      </button>
+    );
+  } else {
+    return (
+      <button
+        type={type}
+        onClick={onClick}
+        className="bg-red-600 hover:bg-red-700 text-white text-lg py-2 rounded-full"
+        style={{ width: "-webkit-fill-available" }}
+      >
+        {children}
+      </button>
+    );
+  }
 }
